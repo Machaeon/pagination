1f9823c597f3c6f9729955c96cbf99b1bc399653
Machaeon <drozhd@gmail.com>
2022-03-24 17:23:59 +0300

    Update pages.php
    Optional remove splitter for single page

--- pages.php
+++ pages.php
@@ -7,16 +7,18 @@ function generatePages(
     $startCount = 1,
     $middleCount = 2,
     $endCount = 1,
-    $strSplit = '...'
+    $strSplit = '...',
+    $removeSinglePageSplitter = true
 ){
     return array_reduce(array_merge(
         range(1, $startCount),
         range(max($currentPage - $middleCount, $startCount + 1), min($currentPage + $middleCount, $pagesCount - $endCount)),
         range($pagesCount - $endCount + 1, $pagesCount)),
-        function($carry, $item) use ($pagesCount, $strSplit) {
+        function($carry, $item) use ($pagesCount, $strSplit, $removeSinglePageSplitter) {
                 if (is_numeric(end($carry)) && $item - end($carry) > 1) {
-                    //$carry[] = $strSplit;
-                    $carry[] = ($item - end($carry) > 2) ? $strSplit : $item - 1;
+                    $carry[] = $removeSinglePageSplitter
+                        ? ($item - end($carry) > 2) ? $strSplit : $item - 1
+                        : $strSplit;
                 }
                 $carry[] = $item;
 
